9f204da97e784dd518c06c9b7b5eae39b5ea0874
hojohnnygithub <ho.johnny@me.com>
2018-10-24 13:55:41 +0800

    Update driving_bot.ino

--- driving_bot.ino
+++ driving_bot.ino
@@ -1,7 +1,9 @@
-//This sketch is for Lesson 6 "Motoring Around" and this will instruct your buggy to drive around.
-
-// the code below connects the motor board pins to Arduino Uno PWM digital pins
-
+/*This sketch is for Lesson 6 "Motoring Around" and this will instruct your buggy to drive around.
+Author - Mr.Johnny Ho
+Last update - 24 Oct
+Updated to include extra details for the MAZEBot Activity(Please ignore unless working on MAZEBot lesson)
+*/
+// the code below connects the motor board pins to Arduino digital pins
 // motor A (wired into the "A" on the motor board)
 
 const int motorpinLeft1 = 5;
@@ -12,6 +14,7 @@ const int motorpinLeft2 = 6;
 const int motorpinRight1 = 11;
 const int motorpinRight2 = 10;
 
+//MAZEBot - include the light sensor pins underneath this line
 
 void setup()
 {
@@ -27,6 +30,9 @@ void setup()
   Serial.begin(9600);
   //send message to the serial monitor
   Serial.println("Setup complete");
+
+//MAZEBot - include the sensor inputs underneath this line but still inside the setup function
+  
 }
 
 // ++++++++++++++++++++++++++++++FUNCTION CODE+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -50,18 +56,6 @@ void motorsForward(int speed, int milliseconds) {
   analogWrite(motorpinRight1, speed*1.0); // the "*1.0" can be changed to calibrate the motor
   delay(milliseconds);
  Serial.println("motors forward"); 
- //call the stopMotors() function
-  stopMotors();
-
-}
-
-void motorsBackward(int speed, int milliseconds) {
-
- 
-  analogWrite(motorpinLeft2, speed*1.0);   // the "*1.0" can be changed to calibrate the motor
-  analogWrite(motorpinRight2, speed*1.0); // the "*1.0" can be changed to calibrate the motor
-  delay(milliseconds);
- Serial.println("motors Backward"); 
  //call the stopMotors() function
   stopMotors();
 
@@ -95,23 +89,20 @@ void pointTurnRight(int speed, int milliseconds){
 //this code will stop your program 
 void shutdown()
 {
- Serial.println("program finished"); 
+ Serial.println("sketch finish"); 
  while(1);
 }
 
-//Add any additional Function code here i.e. light sensor, colour sensor. 
+//MAZEBot - Add your avoidObstacle() function below this line.
 
-
-//++++++++++++++++++++++++++++++++++++++++++++PRIMARY CODE+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//++++++++++++++++++++++++++++++++++++++++++++PRIMARY CODE++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // the code below is your program, instructions here will be completed over and over again unless the "shutdown" function is called.
 void loop()
 {
   //tells your buggy to wait 3 seconds before completing instructions after it
   delay(3000); 
-  //calls the "motorsForward" function and passes the value "200" for speed and the value "1000" milliseconds for time 
+  //calls the "motorsForward" function and passes the value "200" for speed and the value "500" milliseconds for time 
   motorsForward(200, 500);
- pointTurnLeft(200, 350);
-   motorsForward(200, 500);
     
    //Add your own code here: 
   
@@ -123,4 +114,4 @@ void loop()
   shutdown(); 
   
 }
-//+++++++++++++++++++++++++++++++++++++++++++++++END OF PROGRAM++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++END OF PROGRAM++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
